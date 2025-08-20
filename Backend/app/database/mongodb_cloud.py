2d0320cfa9270449c64c1e43019a9325850c19c7
Trương Tiến Anh <truongtienanh16@gmail.com>
2025-08-20 11:00:49 +0700

    update

--- Backend/app/database/mongodb_cloud.py
+++ Backend/app/database/mongodb_cloud.py
@@ -7,9 +7,14 @@ import sys
 import re
 import time
 
-sys.path.append(os.path.dirname(os.path.dirname(__file__)))
+sys.path.append(os.path.dirname(os.path.dirname(os.path.dirname(__file__))))
+from config.config import MONGO_URL, OPENAI_API_KEY
 
-client = MongoClient()
+
+client = MongoClient(MONGO_URL)
 db = client['scan-cv']
 job_collection = db['jobs']
 
+BASE_DIR = os.path.dirname(os.path.abspath(os.path.dirname(__file__)))
+job_path = os.path.join(BASE_DIR, '..', 'raw', 'jobs/preprocessed_data.csv')
+jobs_df = pd.read_csv(job_path)
