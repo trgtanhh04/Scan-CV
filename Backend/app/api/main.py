6728a4ae757c86f53cc14204a544b480e047a535
Trương Tiến Anh <truongtienanh16@gmail.com>
2025-09-12 11:16:52 +0700

    delete temp file

--- Backend/app/api/main.py
+++ Backend/app/api/main.py
@@ -106,8 +106,6 @@ async def query_api(request: QueryRequest):
 
     # invoke flow
     result = enrich_final_answer(state)
-    # print("result", result.get("final_answer"))
-    print(request.provider, request.model)
 
     return {
         "question": request.question,
